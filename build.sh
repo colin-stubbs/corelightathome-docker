4727f8e54602f4124fc85ee3b4645e87c92f7884
Colin Stubbs <colin.stubbs@equatetechnologies.com.au>
2022-08-10 13:58:51 +1000

    Ensure permissions on custom entrypoint are fixed before building

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 docker compose -f ./docker-compose.yml rm
 
 # build the container
+# fix perms on custom entrypoint as these may have been lost...
+chmod 0755 container/docker-entrypoint.sh
 docker compose -f ./docker-compose.yml build
