e6d5870943de53d26db2994ca666858a02ba75cb
Colin Stubbs <3059577+colin-stubbs@users.noreply.github.com>
2023-09-28 18:25:25 +1000

    Only include .example corelight-softsensor.conf in repo

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-cp -v local.zeek.example local.zeek
-cp -v corelight-update/global.yaml.example corelight-update/global.yaml
-cp -v corelight-update/configs/defaultPolicy/db-config.yaml.example corelight-update/configs/defaultPolicy/db-config.yaml
+cp -v -i corelight-softsensor.conf.example corelight-softsensor.conf
+cp -v -i local.zeek.example local.zeek
+cp -v -i corelight-update/global.yaml.example corelight-update/global.yaml
+cp -v -i corelight-update/configs/defaultPolicy/db-config.yaml.example corelight-update/configs/defaultPolicy/db-config.yaml
 
 # EOF
