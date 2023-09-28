1b7fb498646c3d978ec782eff218cafbb023ddd6
Colin Stubbs <3059577+colin-stubbs@users.noreply.github.com>
2023-09-28 16:07:36 +1000

    improve build script

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
-# remove any old containers
+#!/bin/bash
+
+# remove any existing container if something has changed
 docker compose -f ./docker-compose.yml rm
 
 # build the container
 # fix perms on custom entrypoint as these may have been lost...
 chmod 0755 container/docker-entrypoint.sh
+
+# build a new container if necessary
+# NOTE: cached layers may be used if there's no modifications in container/Dockerfile and related files
 docker compose -f ./docker-compose.yml build
+
+# EOF
