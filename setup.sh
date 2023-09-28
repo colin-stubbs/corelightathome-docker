e5c01033d339a6bf1f80dcf0cb04499cd2a62f0a
Colin Stubbs <3059577+colin-stubbs@users.noreply.github.com>
2023-09-28 23:01:33 +1000

    ensure suricata.rules file will exist

--- setup.sh
+++ setup.sh
@@ -5,4 +5,6 @@ cp -v -i local.zeek.example local.zeek
 cp -v -i corelight-update/global.yaml.example corelight-update/global.yaml
 cp -v -i corelight-update/configs/defaultPolicy/db-config.yaml.example corelight-update/configs/defaultPolicy/db-config.yaml
 
+touch suricata-rules/suricata.rules
+
 # EOF
