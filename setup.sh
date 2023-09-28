83d98160ac97c6e5a569f58f840b5cde9ff33eb2
Colin Stubbs <3059577+colin-stubbs@users.noreply.github.com>
2023-09-28 18:21:57 +1000

    Create setup script to copy example files

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp -v local.zeek.example local.zeek
+cp -v corelight-update/global.yaml.example corelight-update/global.yaml
+cp -v corelight-update/configs/defaultPolicy/db-config.yaml.example corelight-update/configs/defaultPolicy/db-config.yaml
+
+# EOF
